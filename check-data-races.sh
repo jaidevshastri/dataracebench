3c33b18b06a67722a1723e8c42d27299003a3c25
Jaidev Shastri <jaidevshastri@vt.edu>
2023-02-22 13:16:26 -0500

    wip: Benchmarking clang.

--- check-data-races.sh
+++ check-data-races.sh
@@ -53,13 +53,21 @@ POLYFLAG="micro-benchmarks/utilities/polybench.c -I micro-benchmarks -I micro-be
 FPOLYFLAG="-Imicro-benchmarks-fortran micro-benchmarks-fortran/utilities/fpolybench.o"
 LANGUAGE="default"
 
+CC="clang"
+VALGRIND="valgrind --tool=massif --stacks=yes"
+CLANG_EXEC="results/exec-clang"
+CLANG_EXEC_VALGRIND="$CLANG_EXEC/valgrind-$(date +"%Y-%m-%d")"
+CLANG_RESULT="results/clang-result-$(date +"%Y-%m-%d")"
+CLANG_EXEC_COUNT=5
+
 help () {
     echo
     echo "Usage: $0 [--run] [--help] language"
     echo
     echo "--help      : this option"
     echo "--small     : compile and test all benchmarks using small parameters with Helgrind, ThreadSanitizer, Archer, Intel inspector."
-    echo "--run       : compile and run all benchmarks with gcc (no evaluation)"
+    echo "--compile-clang       : compile with clang (no run)"    
+    echo "--run       : compile and run all benchmarks with clang (perf evaluation)"
     echo "--run-intel : compile and run all benchmarks with Intel compilers (no evaluation)"
     echo "--helgrind  : compile and test all benchmarks with Helgrind"
     echo "--tsan-clang: compile and test all benchmarks with clang ThreadSanitizer"
@@ -112,17 +120,50 @@ if [[ "$OPTION" == "--small" ]]; then
   scripts/test-harness.sh -t 3 -n 2 -d 32 -l $LANGUAGE -x romp
 fi
 
+if [[ "$OPTION" == "--compile-clang" ]]; then
+    rm -rf $CLANG_EXEC
+    mkdir $CLANG_EXEC
+    if [[ "$LANGUAGE" == "c" || "$LANGUAGE" == "C" ]]; then
+    	for test in $TESTS; do
+            echo "------------------------------------------"
+            echo "COMPILING ($CC): $test"
+            CFLAGS="-O0 -g -Wall -std=c99 -fopenmp"
+            if grep -q 'PolyBench' "$test"; then CFLAGS+=" $POLYFLAG"; fi
+            $CC $CFLAGS "$test" -lm -o $CLANG_EXEC/$(basename $test .c) 
+        done    
+    fi
+    exit
+fi
+
 if [[ "$OPTION" == "--run" ]]; then
+    rm -rf $CLANG_EXEC_VALGRIND
+    mkdir -p $CLANG_EXEC_VALGRIND
+    rm -rf $CLANG_RESULT
+    echo "PROGRAM\tBIN_SIZE(B)\tTEXT_SIZE(B)\tBSS_SIZE(B)\tHEAP_SIZE(B)\tSTACK_SIZE(B)\tEXEC_TIME(uS)"
     if [[ "$LANGUAGE" == "c" || "$LANGUAGE" == "C" ]]; then
     	for test in $TESTS; do
             echo "------------------------------------------"
             echo "RUNNING: $test"
-            CFLAGS="-g -Wall -std=c99 -fopenmp"
-            if grep -q 'PolyBench' "$test"; then CFLAGS+=" $POLYFLAG"; fi
-            gcc $CFLAGS "$test" -lm
-            ./a.out  > /dev/null
+            EXEC_TIME=0
+            for ((n=0;n<$CLANG_EXEC_COUNT;n++))
+            do
+                START_TIME=$(date +%s.%6N)
+                ./$CLANG_EXEC/$(basename $test .c) >> /dev/null
+                END_TIME=$(date +%s.%6N)
+                ELAPTIME=$(echo "scale=6; $END_TIME - $START_TIME" | bc)
+                EXEC_TIME=$(echo "scale=6; $EXEC_TIME + $ELAPTIME" | bc)
+            done
+            EXEC_TIME=$(echo "scale=6; $EXEC_TIME / $CLANG_EXEC_COUNT" | bc)
+            $VALGRIND --massif-out-file=$CLANG_EXEC_VALGRIND/$(basename $test .c).log ./$CLANG_EXEC/$(basename $test .c)
+            BIN_SIZE=$(ls -al $CLANG_EXEC/$(basename $test .c) | awk '{print $5;}')
+            TEXT_SIZE=$(objdump -h $CLANG_EXEC/$(basename $test .c) | grep -w .text | awk '{print "0x" $3;}' | xargs printf "%d")
+            BSS_SIZE=$(objdump -h $CLANG_EXEC/$(basename $test .c) | grep -w .bss | awk '{print "0x" $3;}' | xargs printf "%d")
+            HEAP_SIZE=$(grep mem_heap_B $CLANG_EXEC_VALGRIND/$(basename $test .c).log | sed -e 's/mem_heap_B=\(.*\)/\1/' | sort -g | tail -n 1)
+            STACK_SIZE=$(grep mem_stacks_B $CLANG_EXEC_VALGRIND/$(basename $test .c).log | sed -e 's/mem_stacks_B=\(.*\)/\1/' | sort -g | tail -n 1)
+
+            echo -e "$(basename $test .c)\t$BIN_SIZE\t$TEXT_SIZE\t$BSS_SIZE\t$HEAP_SIZE\t$STACK_SIZE\t$EXEC_TIME" >> $CLANG_RESULT
         done
-        rm -f ./a.out
+        #rm -f ./a.out
 # test for cpp files
     elif [[ "$LANGUAGE" == "c++" || "$LANGUAGE" == "C++" ]]; then
    	for test in $CPPTESTS; do
@@ -239,7 +280,7 @@ if [[ "$OPTION" == "--helgrind" ]]; then
 fi
 
 if [[ "$OPTION" == "--archer" ]]; then
-    scripts/test-harness.sh -t 8 -n 5 -d 32 -l $LANGUAGE -x archer
+    scripts/test-harness.sh -t 8 -n 1 -d 32 -l $LANGUAGE -x archer
 fi
 
 if [[ "$OPTION" == "--coderrect" ]]; then
@@ -247,7 +288,7 @@ if [[ "$OPTION" == "--coderrect" ]]; then
 fi
 
 if [[ "$OPTION" == "--tsan-clang" ]]; then
-    scripts/test-harness.sh -t 8 -n 5 -d 32 -l $LANGUAGE -x tsan-clang
+    scripts/test-harness.sh -t 8 -n 1 -d 32 -l $LANGUAGE -x tsan-clang
 fi
 
 if [[ "$OPTION" == "--tsan-gcc" ]]; then
